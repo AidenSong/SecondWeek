53a44ab0bc4b622a137063035d0d25ee624e5dda
kiset200 <kiset200@jcobinfo.com>
2024-10-03 00:49:00 +0900

    domain.session
    SessionEntity 내용추가

--- src/main/java/com/example/secondweek/specialsession/domain/session/SessionEntity.java
+++ src/main/java/com/example/secondweek/specialsession/domain/session/SessionEntity.java
@@ -4,18 +4,18 @@ package com.example.secondweek.specialsession.domain.session;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.Getter;
 
 import java.time.LocalDate;
 
 @Entity
-@Getter
 public class SessionEntity {
 
     @Id
     private String id;
-    private String name;
-    private String professorName;
-    private LocalDate date;
 
+    private String name;
+
+    private String professorName;
+
+    private LocalDate date;
 }
